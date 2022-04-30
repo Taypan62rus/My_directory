6110a77f2bffaa4e69e377e10c41d86776bf4e5f
Dmitry <dmitry.teleganov@yandex.ru>
2022-04-30 19:05:24 +0300

    It is our first commit

---
+++ Basic JS/script.js
@@ -0,0 +1,20 @@
+'use strict'
+
+let incr = 10,
+    decr = 10;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log("2" == 2);
+console.log("2" === 2);
+
+let isChecked = true;
+    isClose = true;
+    isNewoper = false;
+    isDouble = false;
+
+console.log(isChecked && isClose);
+console.log(isChecked && isNewoper);
+console.log(isChecked || isNewoper);
+console.log(isNewoper || isDouble);
